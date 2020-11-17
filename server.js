e5cd388c63ba836eabc778c557126da3c60564f1
Evan Hanson <hans2624@stthomas.edu>
2020-11-16 19:51:05 -0600

    Added dropdown menu for selecting years to both JS and HTML, added CSS file to make dropdown menu look better in positioning. Added error message for selecting invalid year.

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/year/:selected_year', (req, res) => {
         // this will require a query to the SQL database
         if (err) {
             res.status(404).type('txt');
-            res.write('cannot read dynamic.html');
+            res.write('cannot read year.html');
             res.end();
         } else {
             //res.status(200).type('html').send(template); // <-- you may need to change this
@@ -53,43 +53,58 @@ app.get('/year/:selected_year', (req, res) => {
             let nuclear_total = 0;
             let petroleum_total = 0;
             let renewable_total = 0;
+            let currYear;
+            let yearString = "";
 
-            db.all(sql, [req.params.selected_year], (err, rows) => {
-                //rows.forEach((row) => {
-                    //console.log(row);
-                //});
-                for (row = 0; row < rows.length; row++) {
-                    let total = rows[row].coal + rows[row].natural_gas + rows[row].nuclear + rows[row].petroleum
-                        + rows[row].renewable;
-                    coal_total = coal_total + rows[row].coal;
-                    natural_gas_total = natural_gas_total + rows[row].natural_gas;
-                    nuclear_total = nuclear_total + rows[row].nuclear;
-                    petroleum_total = petroleum_total + rows[row].petroleum;
-                    renewable_total = renewable_total + rows[row].renewable;
-                    
-                    console.log(rows[row]);
-                    rowString = rowString + "<tr>";
-                    rowString = rowString + "<td>" + rows[row].state_abbreviation + "</td>";
-                    rowString = rowString + "<td>" + rows[row].coal + "</td>";
-                    rowString = rowString + "<td>" + rows[row].natural_gas + "</td>";
-                    rowString = rowString + "<td>" + rows[row].nuclear + "</td>";
-                    rowString = rowString + "<td>" + rows[row].petroleum + "</td>";
-                    rowString = rowString + "<td>" + rows[row].renewable + "</td>";
-                    rowString = rowString + "<td>" + total + "</td>";
-                    rowString = rowString + "</tr>";
+            if (req.params.selected_year > 2018 || req.params.selected_year < 1960) {
+                res.status(404).type('txt');
+                res.write(req.params.selected_year + ' is not a valid year');
+                res.end();
+            }
+
+            else {
+                yearString = yearString + '<option value="' + req.params.selected_year + '"></option>';
+                for (currYear = 1960; currYear <= 2018; currYear++) {
+                    if (currYear != req.params.selected_year) {
+                        yearString = yearString + '<option value="' + currYear + '">' + currYear + '</option>';
+                    }
                 }
-                template = template.replace('{{DATA}}', rowString);
-                template = template.replace('{{COAL_TOTAL}}', coal_total);
-                template = template.replace('{{NATURAL_GAS_TOTAL}}', natural_gas_total);
-                template = template.replace('{{NUCLEAR_TOTAL}}', nuclear_total);
-                template = template.replace('{{PETROLEUM_TOTAL}}', petroleum_total);
-                template = template.replace('{{RENEWABLE_TOTAL}}', renewable_total);
-                template = template.replace('{{YEAR}}', year);
-                template = template.replace('{{CURRENT_YEAR}}', year);
 
-                res.status(200).type('html').send(template); 
-                //updateAndSendResponse(rowString, template, res); // <-- you may need to change this
-            });
+                db.all(sql, [req.params.selected_year], (err, rows) => {
+                    for (row = 0; row < rows.length; row++) {
+                        let total = rows[row].coal + rows[row].natural_gas + rows[row].nuclear + rows[row].petroleum
+                            + rows[row].renewable;
+                        coal_total = coal_total + rows[row].coal;
+                        natural_gas_total = natural_gas_total + rows[row].natural_gas;
+                        nuclear_total = nuclear_total + rows[row].nuclear;
+                        petroleum_total = petroleum_total + rows[row].petroleum;
+                        renewable_total = renewable_total + rows[row].renewable;
+                        
+                        console.log(rows[row]);
+                        rowString = rowString + "<tr>";
+                        rowString = rowString + "<td>" + rows[row].state_abbreviation + "</td>";
+                        rowString = rowString + "<td>" + rows[row].coal + "</td>";
+                        rowString = rowString + "<td>" + rows[row].natural_gas + "</td>";
+                        rowString = rowString + "<td>" + rows[row].nuclear + "</td>";
+                        rowString = rowString + "<td>" + rows[row].petroleum + "</td>";
+                        rowString = rowString + "<td>" + rows[row].renewable + "</td>";
+                        rowString = rowString + "<td>" + total + "</td>";
+                        rowString = rowString + "</tr>";
+                    }
+                    template = template.replace('{{YEAR_LINKS}}', yearString);
+                    template = template.replace('{{DATA}}', rowString);
+                    template = template.replace('{{COAL_TOTAL}}', coal_total);
+                    template = template.replace('{{NATURAL_GAS_TOTAL}}', natural_gas_total);
+                    template = template.replace('{{NUCLEAR_TOTAL}}', nuclear_total);
+                    template = template.replace('{{PETROLEUM_TOTAL}}', petroleum_total);
+                    template = template.replace('{{RENEWABLE_TOTAL}}', renewable_total);
+                    template = template.replace('{{YEAR}}', year);
+                    template = template.replace('{{CURRENT_YEAR}}', year);
+
+                    res.status(200).type('html').send(template); 
+                    //updateAndSendResponse(rowString, template, res); // <-- you may need to change this
+                });
+            }
         }
 
     });
